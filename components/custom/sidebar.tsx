140cc6dbe46437539ccdea4dbfdfb4fb7f31b9e6
Franklin Moy <franklinmoy3@gmail.com>
2024-11-12 02:35:28 +0000

    Light refactoring

--- components/custom/sidebar.tsx
+++ components/custom/sidebar.tsx
@@ -5,14 +5,8 @@ import { cn } from '@/lib/utils';
 export function Sidebar({ className }: { className?: string }) {
   return (
     <aside className={cn('w-64 border-r bg-background p-6', className)}>
-      <div className="mt-6 flex flex-col space-y-4">
-        <h2 className="text-lg font-semibold">Location</h2>
-        <GeolocationInput />
-      </div>
-      <div className="mt-6 flex flex-col space-y-4">
-        <h2 className="text-lg font-semibold">Sort Options</h2>
-        <SortOptions />
-      </div>
+      <GeolocationInput />
+      <SortOptions />
     </aside>
   );
 }
--- components/custom/sort-options.tsx
+++ components/custom/sort-options.tsx
@@ -14,17 +14,20 @@ export function SortOptions() {
   const { sortBy, setSortBy } = useGasStationSortContext();
 
   return (
-    <RadioGroup value={sortBy} onValueChange={setSortBy}>
-      {sortOptions.map((option) => (
-        <div key={option.value} className="flex items-center space-x-2">
-          <RadioGroupItem
-            aria-label={option.label}
-            value={option.value}
-            id={option.value}
-          />
-          <Label htmlFor={option.value}>{option.label}</Label>
-        </div>
-      ))}
-    </RadioGroup>
+    <div className="mt-6 flex flex-col space-y-4">
+      <label className="text-lg font-semibold">Sort Options</label>
+      <RadioGroup value={sortBy} onValueChange={setSortBy}>
+        {sortOptions.map((option) => (
+          <div key={option.value} className="flex items-center space-x-2">
+            <RadioGroupItem
+              aria-label={option.label}
+              value={option.value}
+              id={option.value}
+            />
+            <Label htmlFor={option.value}>{option.label}</Label>
+          </div>
+        ))}
+      </RadioGroup>
+    </div>
   );
 }
